7341173032dbb2f8084272168793c56ec382b32a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-08-07 14:04:54 -0700

    Working on the Main entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+// src/App.tsx
+import Routes, Route from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
 
-function App() {
+function HomePage() {
   return (
-    <div className="font-sans">
-      <Navbar />
+    <>
       <Hero />
       <Features />
       <CTA />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
       <Footer />
     </div>
   );
