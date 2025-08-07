386aabd43478bda3a0bf5db0d0c31cda1c479721
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-08-07 13:57:05 -0700

    Working on the Main entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Navbar } from "./components/Navbar";
-import { Hero } from "./components/Hero";
-import { Features } from "./components/Features";
-import { CTA } from "./components/CTA";
-import { Footer } from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
 function App() {
   return (
